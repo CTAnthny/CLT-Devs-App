19b267439f4bba8656faa2b8cd9eff32d3749258
Chris Anthony <CTAnthny@gmail.com>
2017-11-01 18:57:19 -0400

    Style auth forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'main_pages#home'
-
   devise_for :members
 
+  unauthenticated do
+    as :member do
+      root to: 'devise/sessions#new'
+    end
+  end
+
+  # root to dashboard later
+  root to: 'main_pages#home'
 end
