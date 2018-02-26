8e0388bd9351c7993a9a94c61c603a2fa422bcd6
Chris Anthony <CTAnthny@gmail.com>
2018-02-26 07:15:03 -0500

    Custom devise controllers, fail to push tag list entries to own db column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :skills, :interests, :skill_list, :interest_list])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :tag_list])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email])
   end
 end
