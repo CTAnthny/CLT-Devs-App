e09dac40462be5222d3475ab5fd0f51e569d98e0
Chris Anthony <CTAnthny@gmail.com>
2018-03-02 10:36:27 -0500

    Revert to acts_as_taggable, blank 'skill' & 'interests' on model, see 'skill_list' & 'interest_list'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :skill_list, :interest_list])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :tag_list])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :members, controllers: { registrations: 'members/registrations' }
+  devise_for :members
 
   unauthenticated do
     as :member do
